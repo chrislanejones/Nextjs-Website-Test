fc9816bdd45bf4afbb3d1ffe7c4cb8334a81efca
Chris Lane Jones <chrislanejones@gmail.com>
2024-02-14 00:04:03 -0500

    NextJS - route.js - JSON Users Added

--- app/api/users/route.js
+++ app/api/users/route.js
@@ -7,3 +7,16 @@ export async function HEAD(request) {}
 export async function POST(request) {}
 
 export async function PUT(request) {}
+
+export async function DELETE(request) {}
+
+export async function PATCH(request) {}
+
+export async function Get(request) {
+  const users = [
+    { id: 1, name: "John" },
+    { id: 2, name: "Jane" },
+    { id: 3, name: "Bob" },
+  ];
+  return new Response(JSON.stringify(users));
+}
