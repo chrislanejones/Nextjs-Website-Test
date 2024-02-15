bdc5221c1b2091e4023076d81f006d64b7f718a7
Chris Lane Jones <chrislanejones@gmail.com>
2024-02-14 21:59:20 -0500

    NextJS - page.js - Metadata Removed

--- app/posts/page.js
+++ app/posts/page.js
@@ -7,14 +7,8 @@ export async function generateMetadata({ params, searchParams }, parent) {
   // fetch data
   const product = await fetch(`https://.../${id}`).then((res) => res.json());
 
-  // optionally access and extend (rather than replace) parent metadata
-  const previousImages = (await parent).openGraph?.images || [];
-
   return {
     title: product.title,
-    openGraph: {
-      images: ["/some-specific-page-image.jpg", ...previousImages],
-    },
   };
 }
 const page = () => {
