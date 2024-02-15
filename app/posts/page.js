0d1a3788b0e92df09692046ee84c6870a0d18982
Chris Lane Jones <chrislanejones@gmail.com>
2024-02-14 21:56:58 -0500

    NextJS - page.js - Dynamic Metadata

--- app/posts/page.js
+++ app/posts/page.js
@@ -1,7 +1,24 @@
 import React from "react";
 
+export async function generateMetadata({ params, searchParams }, parent) {
+  // read route params
+  const id = params.id;
+
+  // fetch data
+  const product = await fetch(`https://.../${id}`).then((res) => res.json());
+
+  // optionally access and extend (rather than replace) parent metadata
+  const previousImages = (await parent).openGraph?.images || [];
+
+  return {
+    title: product.title,
+    openGraph: {
+      images: ["/some-specific-page-image.jpg", ...previousImages],
+    },
+  };
+}
 const page = () => {
   return <div>posts</div>;
 };
 
-export default page;
+export default function Page({ params, searchParams }) {}
