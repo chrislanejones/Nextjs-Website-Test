9f01e4e51424dd518d56e94a58062a7504bb4a52
Chris Lane Jones <chrislanejones@gmail.com>
2024-02-18 05:24:38 -0500

    Promptopia - Nav.js - Mobile Dropdown

--- promptopia/componates/Nav.jsx
+++ promptopia/componates/Nav.jsx
@@ -4,16 +4,20 @@ import React, { useEffect, useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { signIn, signOut, useSession, getProviders } from "next-auth/react";
+import Provider from "./Provider";
 
 const Nav = () => {
   const isUserLoggedIn = true;
-  const [Providers, setProviders] = useState(null);
+  const [providers, setProviders] = useState(null);
+  const [toggleDropdown, setToggleDropdown] = useState(false);
 
   useEffect(() => {
     const setProviders = async () => {
       const responce = await getProviders();
       setProviders(responce);
     };
+
+    setProviders();
   }, []);
 
   return (
@@ -28,8 +32,6 @@ const Nav = () => {
         />
         <p className="logo_text">promptopia</p>
       </Link>
-
-      {/* Mobile Navigation */}
 
       {/* Desktop Navigation */}
       <div className="sm:flex hidden">
@@ -52,7 +54,59 @@ const Nav = () => {
             </Link>
           </div>
         ) : (
-          <></>
+          <>
+            {providers &&
+              Object.values(providers).map((provider) => (
+                <button
+                  type="button"
+                  key={provider.name}
+                  onClick={() => signIn(provider.id)}
+                  className="black_btn"
+                >
+                  Sign In
+                </button>
+              ))}
+          </>
+        )}
+      </div>
+      {/* Mobile Navigation */}
+      <div className="sm:hidden flex relative">
+        {isUserLoggedIn ? (
+          <div className="flex">
+            <Image
+              src="/assets/images/logo.svg"
+              width={37}
+              height={37}
+              className="rounded-full"
+              alt="user"
+              onClick={() => setToggleDropdown((prev) => !prev)}
+            />
+            {toggleDropdown && (
+              <div className="dropdown">
+                <Link
+                  href="/profile"
+                  className="dropdown_link"
+                  onClick={() => setToggleDropdown(false)}
+                >
+                  My Profile
+                </Link>
+              </div>
+            )}
+          </div>
+        ) : (
+          <>
+            {providers &&
+              Object.values(providers).map((provider) => (
+                <button
+                  type="button"
+                  key={provider.name}
+                  onClick={() => signIn(provider.id)}
+                  className="black_btn"
+                >
+                  Sign In
+                </button>
+              ))}
+          </>
         )}
       </div>
     </nav>
