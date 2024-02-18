8f7395fa14c9c76077b33795eefab4d2933b7440
Chris Lane Jones <chrislanejones@gmail.com>
2024-02-18 04:42:29 -0500

    Promptopia - Creating If Statements for Responsiveness

--- promptopia/componates/Nav.jsx
+++ promptopia/componates/Nav.jsx
@@ -6,6 +6,8 @@ import Image from "next/image";
 import { signIn, SignOut, useSession, getProviders } from "next-auth/react";
 
 const Nav = () => {
+  const isUserLoggedIn = true;
+
   return (
     <nav className="flex-between w-full mb-16 pt-3">
       <Link href="/" className="flex gap-2 flex-center">
@@ -20,7 +22,19 @@ const Nav = () => {
       </Link>
 
       {/* Mobile Navigation */}
-      <div className="sm:flex hidden"></div>
+
+      {/* Desktop Navigation */}
+      <div className="sm:flex hidden">
+        {isUserLoggedIn ? (
+          <div className="flex gap-3 md:gap-5">
+            <Link href="/create-prompt" className="black_btn">
+              Create Post
+            </Link>
+          </div>
+        ) : (
+          <></>
+        )}
+      </div>
     </nav>
   );
 };
