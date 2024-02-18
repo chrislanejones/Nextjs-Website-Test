b133a70a123e43b5c80bab22c5b9c733cc77b057
Chris Lane Jones <chrislanejones@gmail.com>
2024-02-18 04:55:23 -0500

    Promptopia - Nav.js - New Buttons and Set Providers

--- promptopia/componates/Nav.jsx
+++ promptopia/componates/Nav.jsx
@@ -3,10 +3,18 @@
 import React, { useEffect, useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
-import { signIn, SignOut, useSession, getProviders } from "next-auth/react";
+import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 
 const Nav = () => {
   const isUserLoggedIn = true;
+  const [Providers, setProviders] = useState(null);
+
+  useEffect(() => {
+    const setProviders = async () => {
+      const responce = await getProviders();
+      setProviders(responce);
+    };
+  }, []);
 
   return (
     <nav className="flex-between w-full mb-16 pt-3">
@@ -29,6 +37,18 @@ const Nav = () => {
           <div className="flex gap-3 md:gap-5">
             <Link href="/create-prompt" className="black_btn">
               Create Post
+            </Link>
+            <button type="button" onClick={signOut} className="outline_btn">
+              Sign Out
+            </button>
+            <Link href="/profile">
+              <Image
+                src="/assets/images/logo.svg"
+                width={37}
+                height={37}
+                className="rounded-full"
+                alt="user"
+              />
             </Link>
           </div>
         ) : (
