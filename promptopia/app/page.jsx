c2c6867998aee707c0ea54d07a167521a337c456
Chris Lane Jones <chrislanejones@gmail.com>
2024-02-17 23:41:14 -0500

    Promptopia - Adding Sematic HTML and Tailwind Styles

--- promptopia/app/page.jsx
+++ promptopia/app/page.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">Discover & Share</h1>
+    </section>
+  );
 };
 
 export default Home;
