9ed89c3262beae971d5c639d2043752333ae3b3e
Chris Lane Jones <chrislanejones@gmail.com>
2024-02-17 23:59:47 -0500

    Promptopia - Feed Prop added to App

--- promptopia/app/page.jsx
+++ promptopia/app/page.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Feed from "@componates/Feed";
 
 const Home = () => {
   return (
@@ -13,7 +14,7 @@ const Home = () => {
         discover, create and share creative prompts
       </p>
 
-      {/* feed */}
+      <Feed />
     </section>
   );
 };
